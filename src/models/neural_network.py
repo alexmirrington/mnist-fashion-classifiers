2b897e827233f006624226cbf7275b498ab401a8
Alex Mirrington <alexmirrington@gmail.com>
2019-09-11 01:56:12 +1000

    Add NeuralNetworkLayer classes

--- src/models/neural_network.py
+++ src/models/neural_network.py
@@ -1,10 +1,62 @@
-from .classifier import Classifier
+from abc import ABC
 import pandas as pd
+import numpy as np
+import sys
+import random
+
+from .classifier import Classifier
+
+
+class NeuralNetworkLayer:
+
+    def __init__(self, size):
+        # Ensure layer size > 0
+        if size <= 0:
+            raise ValueError('Invalid layer size, expected size > 0.')
+
+        self.size = size
+
+
+class FullyConnectedLayer(NeuralNetworkLayer):
+
+    def __init__(self, size: int, prev: NeuralNetworkLayer = None):
+        super().__init__(size)
+        self.prev = prev
+        self.__randomise()
+
+    def __randomise(self):
+        """
+        Randomly initialise the layer's weights and biases.
+        """
+        self.biases = np.random.standard_normal(self.size)
+        self.weights = np.random.standard_normal((self.size, self.prev.size)) if self.prev is not None else None
+        print(self.biases)
+        print(self.weights)
 
 
 class NeuralNetwork(Classifier):
 
-    def __init__(self):
+    def __init__(self, shape: tuple):
+        # Ensure tuple of ints
+        for e in shape:
+            if not isinstance(e, int):
+                raise TypeError('Invalid type in tuple {}, expected int'.format(shape))
+        
+        self.layers = []
+
+
+    def add_layer(self, layer: NeuralNetworkLayer):
+        """
+        Add a new layer to the network
+        """
+        layer.prev = self.layers[-1]
+        self.layers.append(layer)
+
+
+    def __randomise(self):
+        """
+        Randomly initialise the network's weights and biases.
+        """
         return
 
     def clear(self):

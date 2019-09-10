169d078e513e962ee7b94eceeddc8284aae6ca77
Alex Mirrington <alexmirrington@gmail.com>
2019-09-10 16:14:20 +1000

    Add neural net class

---
+++ src/models/neural_network.py
@@ -0,0 +1,26 @@
+from .classifier import Classifier
+import pandas as pd
+
+
+class NeuralNetwork(Classifier):
+
+    def __init__(self):
+        return
+
+    def clear(self):
+        """
+        Reset all classifier parameters to their defaults.
+        """
+        return
+
+    def predict(self, x: pd.DataFrame) -> pd.DataFrame:
+        """
+        Predict the class of each row of data in x.
+        """
+        return
+
+    def train(self, x: pd.DataFrame, y: pd.DataFrame):
+        """
+        Train the classifier on a dataset x and corresponding labels y.
+        """
+        return

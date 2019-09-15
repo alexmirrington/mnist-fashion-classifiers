bbbdf53ba8181101900adf5789dabeff745f657c
Alex Mirrington <alexmirrington@gmail.com>
2019-09-16 03:34:58 +1000

    Implement SGD

--- src/models/neural_network.py
+++ src/models/neural_network.py
@@ -21,8 +21,20 @@ def d_sig(x: np.array):
     sig_x = sig(x)
     return sig_x * (1 - sig_x)
 
-sigmoid = ActivationFunction(sig, d_sig)
+def th(x: np.array):
+    e_nx = np.exp(-x)
+    e_x = np.exp(x)
+    return (e_x - e_nx)/(e_x + e_nx)
 
+def d_th(x: np.array):
+    tanh_x = th(x)
+    #print(tanh_x)
+    #print(tanh_x**2)
+
+    return 1 - tanh_x**2
+
+sigmoid = ActivationFunction(sig, d_sig)
+tanh = ActivationFunction(th, d_th)
 
 
 class NeuralNetworkLayer:
@@ -51,7 +63,9 @@ class FlatDenseLayer(NeuralNetworkLayer):
     def __init__(self, output_shape: tuple, activation=sigmoid):
         super().__init__(output_shape)
         self.activation = activation
-        
+        self.raw_outputs = None
+        self.outputs = None
+
     @classmethod
     def from_size(cls, size: int):
         return cls((size,))
@@ -67,11 +81,13 @@ class FlatDenseLayer(NeuralNetworkLayer):
             self.biases = np.random.standard_normal((self.output_shape[0], 1))
             self.weights = np.random.standard_normal((self.output_shape[0], self.input_shape[0]))
 
-    def get_output(self, ipt: np.ndarray):
+    def get_activations(self, ipt: np.ndarray):
         # No input_shape implies first layer, just return the input as the output
         if self.input_shape is None:
             if ipt.shape != self.output_shape:
                 raise ValueError('Invalid shape of ipt array, expected {} but got {}'.format(self.output_shape, ipt.shape))
+            self.outputs = ipt
+            self.raw_outputs = ipt
             return ipt
 
         # Ensure input is same shape as self.input_shape
@@ -79,21 +95,58 @@ class FlatDenseLayer(NeuralNetworkLayer):
             raise ValueError('Invalid shape of ipt array, expected {} but got {}'.format(self.input_shape, ipt.shape))
 
         ipt = ipt[:, np.newaxis]
-        ret = self.activation.func(self.weights @ ipt + self.biases)
-        ret = ret.squeeze()
+        raw_out = self.weights @ ipt + self.biases
 
-        return ret
+        # TODO Consider memory usage here
+        self.raw_outputs = raw_out.squeeze()
+        self.outputs = self.activation.func(raw_out).squeeze()
+
+        #print(str(min(self.raw_outputs)) + ', ' + str(max(self.raw_outputs)))
+
+        return self.outputs
+
+    def adjust_weights(self, eta: float, prev_outputs: np.ndarray, dC_da: np.ndarray):
+
+        # TODO Validate shapes of errors
+        # errors should have dimension 1xn where n is the number of nodes in the layer above
+        
+        # Compute weight changes using chain rule
+        d_sigma = self.activation.d_func(self.raw_outputs)
+
+        dC_dz = dC_da * d_sigma  # a = sigma(z) so da_dz = d_sigma(z)
+        dw = eta * (np.outer(dC_dz, prev_outputs))  # prev_outputs = dz_dw
+        
+        # Compute bias changes
+        db = eta * dC_dz
+        db = db[:, np.newaxis]
+
+        next_dC_da = np.dot(self.weights.T, d_sigma)
+
+        self.weights += dw
+        self.biases += db
+
+        return next_dC_da
 
 
 class NeuralNetwork(Classifier):
 
-    def __init__(self, layers: list):
+    def __init__(self, layers: list, eta=0.05, batch_size=1):
         # Ensure layers is a ist of NeuralNetworkLayer objects
         for l in layers:
             if not isinstance(l, NeuralNetworkLayer):
                 raise TypeError('Invalid type in list {}, expected {}'.format(layers, NeuralNetworkLayer))
         
+        # Ensure batch_size is > 0
+        if type(batch_size) != int:
+            raise TypeError('Invalid batch_size type, expected {}'.format(int))
+        if batch_size <= 0:
+            raise ValueError('Parameter batch_size must be positive')
+
+        # TODO Validate eta
+
         self.layers = layers
+        self.eta = eta
+        self.batch_size = batch_size
 
         # Link layers
         i = 1
@@ -130,13 +183,47 @@ class NeuralNetwork(Classifier):
     def __predict_single(self, row):
         ipt = row
         for layer in self.layers:
-            ipt = layer.get_output(ipt)
-        return ipt
+            ipt = layer.get_activations(ipt)
+        return ipt        
+
+    # @staticmethod
+    # def cost(predicted: np.ndarray, actual: np.ndarray):
+    #     return ((actual - predicted)**2).mean(axis=ax)
 
     def train(self, x: pd.DataFrame, y: pd.DataFrame):
         """
         Train the classifier on a dataset x and corresponding labels y.
         """
-        # TODO: Implement this
-        return
-        
+        # TODO implement batch sizes, allowing for batch prediction too
+        epochs = 10
+        for e in range(epochs):
+            print('Epoch: {}'.format(e))
+
+            epoch_correct = 0
+
+            index = 0
+            while index < len(x):
+
+                y_pred = self.__predict_single(x[index])
+                # TODO Fix assumption that number of output neurons is the same
+                # as the number of classes
+                y_actual = np.zeros(y_pred.shape)
+                y_actual[y[index]] = 1
+                dC_da = y_actual - y_pred
+
+                epoch_correct += 1 if np.argmax(y_pred) == y[index] else 0
+
+                # iterate through layers, propagating errors
+                layer = len(self.layers) - 1
+                while layer > 0:
+                    dC_da = self.layers[layer].adjust_weights(self.eta, self.layers[layer-1].outputs, dC_da)
+                    layer -= 1
+
+                index += 1
+
+                
+            print('Train accuracy: {:.2f}%'.format(epoch_correct/index * 100))
+
+        print('Training complete')
+
+            

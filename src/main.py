3f7da35d210d6dbc5f1bb21d9e80376ef15f3a52
Alex Mirrington <alexmirrington@gmail.com>
2019-10-01 21:23:38 +1000

    Reformat test and validation sets

--- src/main.py
+++ src/main.py
@@ -2,28 +2,39 @@ import h5py
 import numpy as np
 
 from models.classifier import Classifier
-from models.neural_network import NeuralNetwork
-from models.neural_network import FlatDenseLayer
+from models.naive_bayes import MultinomialNaiveBayes
+from models.neural_network import NeuralNetwork,FlatDenseLayer
 from utils.functions import sigmoid, tanh, relu, softplus
 from utils.metrics import accuracy
 
 
 def main():
-    # Load data
-    with h5py.File('./data/train/images_training.h5', 'r') as h:
-        data_train = np.copy(h['datatrain'])
+    train_data, train_labels, test_data, test_labels = load_data()
 
-    with h5py.File('./data/train/labels_training.h5', 'r') as h:
-        label_train = np.copy(h['labeltrain'])
+    naive_bayes(train_data, train_labels, test_data, test_labels)
+    neural_net(train_data, train_labels, test_data, test_labels)
 
-    with h5py.File('./data/test/images_testing.h5', 'r') as h:
-        data_test_all = np.copy(h['datatest'])
 
-    with h5py.File('./data/test/labels_testing_2000.h5', 'r') as h:
-        label_val = np.copy(h['labeltest'])
+def naive_bayes(train_data, train_labels, test_data, test_labels):
 
-    data_val = data_test_all[:2000]
+    # Create and train model
+    model = MultinomialNaiveBayes()
+    model.train(train_data, train_labels)
+    
+    # Predict 2000 validation set samples and calculate accuracy
+    test_data_2k = test_data[:len(test_labels)]
+    test_pred = model.predict(test_data_2k)
+    print('Test accuracy: {:.02f}%'.format(100*accuracy(test_pred, test_labels)))
 
+    # Predict 10000 test set samples and save predictions
+    test_pred = model.predict(test_data)
+    print(len(test_pred))
+    save_predictions(naive_bayes.__name__, test_pred)
+
+
+def neural_net(train_data, train_labels, test_data, test_labels):
+
+    # Create and train model
     model = NeuralNetwork([
         FlatDenseLayer((784,), activation=sigmoid),
         FlatDenseLayer((100,), activation=sigmoid),
@@ -31,10 +42,38 @@ def main():
         FlatDenseLayer((10,), activation=sigmoid),
     ], eta=0.1, batch_size=64)
 
-    model.train(data_train, label_train, epochs=50)
-    y_activations, y_pred = model.predict(data_val)
+    model.train(train_data, train_labels, epochs=50)
 
-    print('Accuracy: {:.02f}%'.format(100*accuracy(y_pred,label_val)))
+    # Predict 2000 validation set samples and calculate accuracy
+    test_data_2k = test_data[:len(test_labels)]
+    test_activations, test_pred = model.predict(test_data_2k)
+    print('Test accuracy: {:.02f}%'.format(100*accuracy(test_pred, test_labels)))
+
+    # Predict 10000 test set samples and save predictions
+    test_activations, test_pred = model.predict(test_data)
+    print(len(test_pred))
+    save_predictions(neural_net.__name__, test_pred)
+
+
+def load_data():
+    with h5py.File('./data/train/images_training.h5', 'r') as h:
+        train_data = np.copy(h['datatrain'])
+
+    with h5py.File('./data/train/labels_training.h5', 'r') as h:
+        train_labels = np.copy(h['labeltrain'])
+
+    with h5py.File('./data/test/images_testing.h5', 'r') as h:
+        test_data = np.copy(h['datatest'])
+
+    with h5py.File('./data/test/labels_testing_2000.h5', 'r') as h:
+        test_labels = np.copy(h['labeltest'])
+
+    return train_data, train_labels, test_data, test_labels
+
+
+def save_predictions(filename, predictions):
+    with h5py.File('./results/' + filename + '.h5', 'w') as h:
+        h.create_dataset('output', data=predictions)
 
 
 if __name__ == '__main__':

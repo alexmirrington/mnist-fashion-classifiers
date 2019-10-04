4df55f5925442ddf200de64aeb464221a18ee323
Alex Mirrington <alexmirrington@gmail.com>
2019-10-04 11:42:46 +1000

    Add binary class preprocessing func

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -5,3 +5,16 @@ def shuffle_data(x: np.ndarray, y:  np.ndarray):
     data = np.concatenate((x, y), axis=1)
     np.random.shuffle(data)
     return data[:,:-1], data[:,data.shape[1]-1:].squeeze()
+
+def binary_partition_by_class(x: np.ndarray, y: np.ndarray, partition_map: set):
+    classes = np.unique(y)
+    for c in partition_map:
+        if c not in classes:
+            raise ValueError('Value {} in parition_map was not found in y.'.format(c))
+
+    partition = y
+    for c in partition_map:
+        partition[partition==c] = 0
+        partition[partition!=c] = 1
+
+    return x, partition
